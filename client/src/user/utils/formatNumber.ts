becc7456760c2b3e0a1a4511a05ff9ea64e4b9fa
tranvantritvt <tranvantri270802@gmail.com>
2024-05-15 10:46:43 +0700

    delete client

--- client/src/user/utils/formatNumber.ts
+++
@@ -1,7 +0,0 @@
-// chuyển tiền về dạng VND (60000 => 60.000 đ)
-export const formatNumber = (price: number) => {
-  return new Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND",
-  }).format(price);
-};
